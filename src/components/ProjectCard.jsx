adfe5afcdbc71dfefefe032b3b1be7fc24b7ebbc
jonathanalaksana <jonathanalaksana@gmail.com>
2022-07-18 19:05:47 -0700

    add project images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea, CardActions, IconButton } from "@mui/material";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  IconButton,
+} from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 function ProjectCard({ name, img, description, github }) {
   return (
     <Card variant="outlined" sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          src={process.env.PUBLIC_URL + img}
-          alt={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        src={process.env.PUBLIC_URL + img}
+        alt={name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
       <CardActions>
         <IconButton
           component="a"
--- src/projects.js
+++ src/projects.js
@@ -11,33 +11,33 @@ const projects = [
     name: "Woods Humane Volunteer Scheduler",
     image: "/woodsHumane.svg",
     description:
-      "Full stack scheduler application with React and Node.js for a SLO-based non-profit.",
+      "Full stack scheduler application with React, Express, Node.js, and MongoDB for a SLO-based non-profit.",
     github: "https://github.com/hack4impact-calpoly/HumaneSociety",
   },
   {
     name: "Speech Emotion Recognition",
-    image: "image link",
+    image: "/ser.png",
     description:
-      "Machine learning models created with Tensorflow to recognize emotion in speech.",
+      "Machine learning models created with Tensorflow to recognize seven emotions in speech recordings.",
     github: "https://github.com/jlaksana/Speech-Emotion-Recognition",
   },
   {
     name: "Tic-Tac-Toe Game and AI",
-    image: "image link",
+    image: "ttt_ai.png",
     description:
-      "Tic-Tac-Toe GUI with PyGame and an unbeatable AI using the Minimax algorithm.",
+      "Tic-Tac-Toe GUI with PyGame and an unbeatable AI using the Minimax algorithm with alpha-beta pruning.",
     github: "https://github.com/jlaksana/TicTacToe-Game-and-AI",
   },
   {
     name: "Maze Solver",
-    image: "image link",
+    image: "maze_solver.png",
     description:
       "Console program that solves mazes using DFS, BFS, Dijkstra's, and A Star from an input image. Outputs the solved maze the solution path highlighted.",
     github: "https://github.com/jlaksana/Maze-Solver",
   },
   {
     name: "Sudoku Game and Solver",
-    image: "image link",
+    image: "sudoku.png",
     description:
       "Interactive Sudoku game using the Pygame module. Can visually solve sudoku puzzles using backtracking.",
     github: "https://github.com/jlaksana/Sudoku-Game-and-Solver",
