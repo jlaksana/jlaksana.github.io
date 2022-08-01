f802e4664b79bf2e293d6d907a75c9aef75b9eed
jonathanalaksana <jonathanalaksana@gmail.com>
2022-08-01 16:26:28 -0700

    about me final

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,21 +4,45 @@ import "../css/About.css";
 import headshot from "../images/headshot.jpg";
 
 function About() {
+  // basic info, experiences, mission, hobbies, resume
   return (
     <div id="about">
       <img src={headshot} alt="My headshot" height="300px" />
-      <div>
-        <Typography variant="h4">About Me</Typography>
-        <Typography variant="body1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Typography>
-      </div>
+      <Typography variant="h4">About Me</Typography>
+      <Typography variant="body1">
+        Hi there, my name is Jonathan Laksana! I was born and raised in
+        Sacramento, CA, and currently studying computer science at Cal Poly SLO.
+        I am passionate about working with others to create robust and scalable
+        software solutions. The current areas of computer science that I have
+        been exploring are web development and machine learning.
+      </Typography>
+      <Typography variant="body1">
+        My software development journey began in high school when I built a
+        small maze-solving robot. Building the hardware was cool but the
+        software programming was by far the most fascinating to me. During that
+        project, I discovered that software engineering was what I wanted to do
+        for my career. I have interned at Workday as a Software Application
+        Engineer on their HCM product. There I was first exposed to the
+        professional software engineering discipline. Also, I am a volunteer
+        software developer for the Cal Poly Hack4Impact chapter. There, a team
+        and I developed a full stack scheduler app. Outside of these
+        experiences, I have developed many other projects to expand my skill
+        set. These are viewable below.
+      </Typography>
+      <Typography variant="body1">
+        Thank you for visiting my portfolio! For any questions or inquiries,
+        please contact me at jlaksana@calpoly.edu. You may also view my resume{" "}
+        <a
+          href="https://drive.google.com/file/d/1NRMeL_bOOHDmxIE4lxlELO2si8PuJk6h/view"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Typography variant="body1" display="inline">
+            here
+          </Typography>
+        </a>
+        .
+      </Typography>
     </div>
   );
 }
